5c6a3dfd7e2a164ea25e21d8a1e83113c40d90e8
Achmad Zacky Rachmatullah <achmadzackyr@gmail.com>
2022-06-02 00:59:56 +0700

    Add google analytic kode

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,27 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-30151021-11" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'UA-30151021-11', { page_path: window.location.pathname });
+            `
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
